055d928715c5f59b83e1fcacf1fc9fb955e76fe1
Liu233w <wwwlsmcom@outlook.com>
2018-03-08 16:36:12 +0800

    更新 Receive.h 的注释

--- natchat/Resource.h
+++ natchat/Resource.h
@@ -34,16 +34,41 @@
 #define IDC_PRIVATETEXT                 1030
 #define IDC_REFRESHING                  1031
 
+/// <summary>
+/// 完成文件发送
+/// </summary>
 #define IDC_SEND_FILE_DONE				1101
+/// <summary>
+/// 文件发送失败
+/// </summary>
 #define IDC_SEND_FILE_ERROR				1102
+/// <summary>
+/// 消息发送失败
+/// </summary>
 #define IDC_SEND_MESSAGE_ERROR			1103
+/// <summary>
+/// 消息接收失败
+/// </summary>
 #define IDC_RECEIVE_MESSAGE_ERROR       1104
-
+/// <summary>
+/// 在接收到单发或群发消息的时候触发。建议读取 history 列表并刷新
+/// </summary>
 #define IDC_RECOMMEND_REFRESH_HISTORIES 1105
-
+/// <summary>
+/// 接收到 TIC 的时候触发，建议刷新 User 列表
+/// </summary>
 #define IDC_RECEIVE_TIC					1111
+/// <summary>
+/// 接收到 TOC 的时候触发。可以不处理，在刷新的几秒钟结束之后统一刷新 User 列表
+/// </summary>
 #define IDC_RECEIVE_TOC					1112
+/// <summary>
+/// 广播 TIC 失败。说明点击刷新按钮之后的操作失败了，这时候应该停止主窗口的刷新状态
+/// </summary>
 #define IDC_BROADCAST_TIC_ERROR			1113
+/// <summary>
+/// 接收 TIC 的过程失败了，可以不处理。
+/// </summary>
 #define IDC_RECEIVE_TIC_ERROR			1114
 
 // Next default values for new objects
